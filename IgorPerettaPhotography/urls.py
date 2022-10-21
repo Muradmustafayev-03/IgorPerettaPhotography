1cfbe4deab554d937d1ed15035051682641f6506
Murad <muradmustafayev03@gmail.com>
2022-10-21 22:10:54 +0400

    views and templates

--- IgorPerettaPhotography/urls.py
+++ IgorPerettaPhotography/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from PhotographyProfile import views
+
+app_name = 'main'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls, name='admin'),
+    path('home/', views.home, name='home'),
+    path('register/', views.register_request, name='register'),
 ]
--- PhotographyProfile/views.py
+++ PhotographyProfile/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
-def register(request):
-    pass
+def home(request):
+    return render(request, 'home.html')
 
 
-def login(request):
+def redirect_home(request):
+    return redirect('home')
+
+
+def register_request(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('home')
+        messages.error(request, 'Unsuccessful registration. Invalid information.')
+    form = RegisterForm()
+    return render(request=request, template_name='register.html', context={'register_form': form})
+
+
+def login_request(request):
     pass
 
 
